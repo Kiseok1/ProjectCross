8ff24c03af37a5d78eb3861715cd6ccca29b20ce
studingAshley <143576435+studingAshley@users.noreply.github.com>
2024-01-31 10:24:12 +0900

    Analystic
    분석도구 추가

--- html_sample_02/src/main/java/com/java/controller/FController.java
+++ html_sample_02/src/main/java/com/java/controller/FController.java
@@ -1,9 +1,12 @@
 package com.java.controller;
 
 import org.apache.catalina.Session;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -33,6 +36,35 @@ public class FController {
 	}
 	
 	
+	@RequestMapping("location")
+	@ResponseBody
+	public String location()
+	{
+		String param ="Location";
+		
+		return param;
+	}
+	
+	@RequestMapping("analystic")
+	public String analystic() {
+		return "analystic";
+	}
+	
+	@PostMapping("selectedAnalysis")
+	@ResponseBody
+	public JSONObject selectedAnalysis(int post_id)
+	{
+		JSONObject jobj = new JSONObject();
+
+		jobj.put("viewhit0", "10");
+		jobj.put("viewhit6", "25");
+		jobj.put("viewhit12", "30");
+		jobj.put("viewhit18", "35");
+
+		
+		return jobj;
+	}
+
 	
 	
 	
